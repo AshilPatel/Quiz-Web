6e162a7021059228268e2502a08bdd302a988c36
AshilPatel <65237607+AshilPatel@users.noreply.github.com>
2022-07-28 09:47:00 +0530

    Add files via upload

---
+++ JS/home.js
@@ -0,0 +1,40 @@
+function fun101()
+{
+    var mywindow=window.open("homepage.html","_self");
+}
+function fun102()
+{
+    var mywindow2=window.open("Contact Us.html","_self");
+}
+function fun1()
+{
+    var mywindow1=window.open("HTML.html","_self");
+}
+function fun2()
+{
+    var mywindow1=window.open("CSS.html","_self");
+}
+function fun3()
+{
+    var mywindow1=window.open("JAVASCRIPT.html","_self");
+}
+function fun4()
+{
+    var mywindow1=window.open("PHP.html","_self");
+}
+
+
+
+
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
